5c4bf73b5ef4a60f0eea84996c9027a9004a3803
Eddy_sin <101008331+EddysonSinclair@users.noreply.github.com>
2023-12-08 11:53:04 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 			if (*format == '%')
 			{
 				++format;
-				if (format != '\0')
+				if (*format != '\0')
 					output_handler(format, ap);
 			}
 			else
