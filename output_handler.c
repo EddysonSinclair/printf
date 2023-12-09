ddb61c4d7be6fe4b916d6e95e9cefdfe917ee9d3
Kibet9827 <kibetkeiyo@gmail.com>
2023-12-08 17:03:23 -0800

    minutes

--- output_handler.c
+++ output_handler.c
@@ -9,66 +9,48 @@
 
 int output_handler(const char *specifier, va_list ap)
 {
-	int count, c;
-	char *s;
+	int count;
 
 	count = 0;
-	switch (*specifier)
+	if (*specifier == 'c')
+		count += _putchar(va_arg(ap, int));
+	else if (*specifier == 's')
+		count += printstring(va_arg(ap, char*));
+	else if (*specifier == 'i')
+		count += print_integer(ap);
+	else if (*specifier == 'd')
+		count += print_integer(ap);
+	else if (*specifier == 'u')
+		count += print_decimal_unsignedint(ap);
+	else if (*specifier == 'o')
+		count += print_octal(ap);
+	else if (*specifier == 'x')
+		count += print_hexadecimal(ap, 0);
+	else if (*specifier == 'X')
+		count += print_hexadecimal(ap, 1);
+	else if (*specifier == 'b')
+		count += print_binary(ap);
+	else if (*specifier == 'R')
 	{
-		case '%':
-			_putchar('%');
-			break;
-		case 'c':
-			c = va_arg(ap, int);
-			{
-				_putchar(c);
-				count++;
-			}
-			break;
-		case 's':
-			s = va_arg(ap, char*);
-			{
-				printstring(s);
-				count++;
-			}
-			break;
-		case 'u':
-			count += print_decimal_unsignedint(ap);
-			break;
-		case 'r':
-			{
-				char *str_arg = va_arg(ap, char*);
-				reverse_string(str_arg);
-				count += printstring(str_arg);
-			}
-			break;
-		case 'R':
-			{
-				char *str_arg = va_arg(ap, char*);
-				rot13(str_arg);
-				count += printstring(str_arg);
-			}
-			break;
-		case 'o':
-			count += print_octal(ap);
-			break;
-		case 'x':
-			count += print_hexadecimal(ap, 0);
-			break;
-		case 'X':
-			count += print_hexadecimal(ap, 1);
-			break;
-		case 'i':
-		case 'd':
-			count += print_integer(ap);
-			break;
-		case '\0':
-			return (-1);
-		default:
-			 _putchar('x');
-			count++;
-			break;
+		char *str_arg = va_arg(ap, char*);
+		rot13(str_arg);
+		count += printstring(str_arg);
 	}
+	else if (*specifier == 'r')
+	{
+		char *str_arg = va_arg(ap, char*);
+		reverse_string(str_arg);
+		count += printstring(str_arg);
+	}
+	else if (*specifier == '\0')
+		return (-1);
+	else if (*specifier == '%')
+	{
+		count += _putchar('%');
+		count += write(STDOUT_FILENO, &(*specifier), 1);
+	}
+	else
+		count += write(1, &(*specifier), 1);
 	return (count);
 }
 
