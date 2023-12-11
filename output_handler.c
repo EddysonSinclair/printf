e0c0665aa54572588c50c1983bd74065c7d61830
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-12-11 02:14:57 +0000

    everything changes

--- output_handler.c
+++ output_handler.c
@@ -2,7 +2,7 @@
 
 /**
  * output_handler- handles the output
- * @specifier: conversion specifier
+ * @format: conversion specifier
  * @ap: argument pointer
  * Return: zero
  */
@@ -11,33 +11,30 @@ int output_handler(const char *format, va_list ap)
 {
 	int count;
 	char c, *str;
+
 	count = 0;
-	switch (*format) 
-		{
-			case 'c':
+	switch (*format)
+	{
+		case 'c':
 			c = (char)va_arg(args, int);
 			_putchar(c);
 			count++;
 			break;
-
-			case 's':
+		case 's':
 			str =  va_arg(args, char *);
-			while(*str != '\0')
-				{
-					_putchar(*str);
-					str++;
-					count++;
-				}
-				break;
-
-			case '%':
+			while (*str != '\0')
+			{
+				_putchar(*str);
+				str++;
+				count++;
+			}
+			break;
+		case '%':
 			_putchar('%');
 			count++;
 			break;
-
-			default:
+		default:
 			break;
-		}
-	
+	}
+	return (0);
 }
-
