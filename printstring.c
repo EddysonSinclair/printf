bee6bf67599648dfbd82cbe0f7d7dc72dd7c0a74
Eddysonsinclair <eddysonsinclair@gmail.com>
2023-12-22 13:40:20 +0000

    printf

--- printstring.c
+++ printstring.c
@@ -1,29 +1,25 @@
 #include "main.h"
 /**
  * printstring - print a string.
- * @ap: argumen t.
+ * @ap: argument.
  * Return: the length of the string.
  */
-
 int printstring(va_list ap)
 {
-	char *s;
-	int i, len;
+    char *s;
+    int i, len;
 
-	s = va_arg(ap, char *);
-	if (s == NULL)
-	{
-		s = "(null)";
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-	else
-	{
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
+    s = va_arg(ap, char *);
+    if (s == NULL)
+    {
+        s = "(null)";
+    }
+
+    len = _strlen(s);
+    for (i = 0; i < len; i++)
+    {
+        _putchar(s[i]);
+    }
+
+    return len;
 }
